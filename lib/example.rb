3bb38623240b8529499bd180d948d9c0ae504ca7
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-27 17:25:28 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,12 @@
 # Use this space to code along with the readme
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def greeting_ten_times
+  phrase= "Hello World"
+  puts phrase
+end
+
+greeting_ten_times
+greeting_ten_times
+greeting_ten_times
+greeting_ten_times
+greeting_ten_times
